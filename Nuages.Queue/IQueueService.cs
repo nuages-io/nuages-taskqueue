76c4b6aa0277dc7aa13a284e330e5ac80cdbbde2
martin <Wingman123*>
2021-11-01 19:24:20 -0400

    ok

--- Nuages.Queue/IQueueService.cs
+++ Nuages.Queue/IQueueService.cs
@@ -3,9 +3,7 @@
 public interface IQueueService
 {
     Task<string?> GetQueueFullNameAsync(string queueName);
-    // ReSharper disable once UnusedMember.Global
     Task<string?> EnqueueMessageAsync(string queueFullName, string text);
     Task<List<QueueMessage>> DequeueMessageAsync(string queueFullName, int maxMessages = 1);
-
     Task DeleteMessageAsync(string queueFullName, string id, string receiptHandle);
 }
